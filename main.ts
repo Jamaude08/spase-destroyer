3bc90f5f7886df99ce743a4a463bf17a55b7126c
Jamaude08 <90931807+Jamaude08@users.noreply.github.com>
2021-09-17 14:07:06 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.disintegrate)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    scene.cameraShake(4, 500)
+    otherSprite.destroy(effects.disintegrate)
+    sprite.startEffect(effects.fire, 200)
+    info.changeLifeBy(-1)
+})
+let projectile: Sprite = null
+music.playTone(262, music.beat(BeatFraction.Half))
+scene.setBackgroundColor(scene.backgroundColor())
+info.setLife(3)
+effects.starField.startScreenEffect()
+game.onUpdateInterval(500, function () {
+    let asteroids: Image[] = []
+    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
+    projectile.setKind(SpriteKind.Enemy)
+    projectile.x = randint(10, 150)
+})
